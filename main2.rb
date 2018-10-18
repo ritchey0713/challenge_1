d094832095c2e889b6a0bc42a417c8ee0d05f5ed
ritchey0713 <ritchey0713@gmail.com>
2018-10-18 14:50:00 -0400

    who knows

--- main2.rb
+++ main2.rb
@@ -8,17 +8,26 @@ def parse_file(file)
 end
 
 def extract_data(file)
+  driver_name = []
   output = []
   parse_file(file).map do |new_line|
     if new_line.include?("Driver")
-      output << get_drivers(new_line)
+      driver_name << get_drivers(new_line)
     elsif new_line.include?("Trip")
       output << get_trips(new_line)
     end
-
   end
-  binding.pry
-end
+
+ # output = ["Dan: 17 @ 34 mph.",
+ #"Dan: 21 @ 63 mph.",
+ #"Alex: 42 @ 34 mph."]
+
+#  driver_name=
+# ["Dan: 0 miles",
+#   "Alex: 0 miles",
+#   "Bob: 0 miles"]
+  alter_output(output, driver_name)
+ end
 
 def get_drivers(driver)
   driver  = driver.split(" ")[1..-1].join(" ")
@@ -31,7 +40,18 @@ def get_trips(trip)
   trip_distance = trip[4].to_i
   trip_duration = ((Time.parse(trip[3]) - Time.parse(trip[2])) / 3600)
   average_speed = (trip_distance / trip_duration).round
-  return "#{trip_driver}: #{trip_distance} @ #{average_speed} mph."
+  return "#{trip_driver}: #{trip_distance} miles @ #{average_speed} mph."
+end
+
+def alter_output(array, driver_name)
+  i = 0
+new_name = driver_name.map do |name|
+name = name.split(":")[0]
+  array.select{|arr| arr.include?(name)}
+end
+
+  # if new_name[].length > 1
+  #   return new_name[]
 end
 
 extract_data("seed_data.txt")
