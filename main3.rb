13a5c31d507ff305394706fe4b15bcd5e0972bd0
ritchey0713 <ritchey0713@gmail.com>
2018-10-21 11:43:30 -0400

    fix indents

--- main3.rb
+++ main3.rb
@@ -7,6 +7,41 @@ def parse_file(file)
   lines = lines.join.split("\n")
 end
 
+def get_drivers(driver)
+  driver  = driver.split(" ")[1..-1].join(" ")
+  return driver
+end
+
+  def get_trips(trip)
+    trip = trip.split(" ")
+    trip_distance = trip[4].to_f
+    trip_duration = ((Time.parse(trip[3]) - Time.parse(trip[2])) / 3600)
+    average_speed = (trip_distance / trip_duration)
+    return {distance: trip_distance, time: trip_duration, speed: average_speed}
+  end
+
+def assign_trips(hash, driver, new_line)
+  if hash.keys.include?(driver)
+    hash[driver] << get_trips(new_line)
+  end
+end
+
+def average_trips(hash)
+  hash.map do |driver|
+    driver[1].inject do |a, b|
+      result = {}
+      result[:distance] = a[:distance] + b[:distance]
+      result[:time] = a[:time] + b[:time]
+      result[:speed] = (result[:distance] / result[:time])
+      hash[driver[0]] = result
+    end
+  end
+end
+
+def print_trips
+
+end
+
 def extract_data(file)
   hash = {}
   parse_file(file).map do |new_line|
@@ -19,41 +54,7 @@ def extract_data(file)
     end
   end
     average_trips(hash)
+    print_trips(hash)
 end
 
-  def get_drivers(driver)
-    driver  = driver.split(" ")[1..-1].join(" ")
-    return driver
-  end
-
-  def get_trips(trip)
-    trip = trip.split(" ")
-    trip_distance = trip[4].to_f
-    trip_duration = ((Time.parse(trip[3]) - Time.parse(trip[2])) / 3600)
-    average_speed = (trip_distance / trip_duration)
-    return {distance: trip_distance, time: trip_duration, speed: average_speed}
-  end
-
-  def assign_trips(hash, driver, new_line)
-    if hash.keys.include?(driver)
-      hash[driver] << get_trips(new_line)
-    end
-  end
-
-  def average_trips(hash)
-    hash.map do |driver|
-      driver[1].inject do |a, b|
-        result = {}
-        result[:distance] = a[:distance] + b[:distance]
-        result[:time] = a[:time] + b[:time]
-        result[:speed] = (result[:distance] / result[:time])
-        hash[driver[0]] = result
-      end
-    end
-  end
-
-  def print_trips
-
-  end
-
-  extract_data("seed_data.txt")
+extract_data("seed_data.txt")
