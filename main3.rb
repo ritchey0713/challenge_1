afaebf362d3784035ef28e1106de270e7cbe2f59
ritchey0713 <ritchey0713@gmail.com>
2018-10-21 20:19:13 -0400

    rename methods

--- main3.rb
+++ main3.rb
@@ -7,24 +7,24 @@ def parse_file(file)
   lines = lines.join.split("\n")
 end
 
-def get_drivers(driver)
+def get_driver_names(driver)
   driver  = driver.split(" ")[1..-1].join(" ")
   return driver
 end
 
-  def get_trips(trip)
-    trip = trip.split(" ")
-    trip_distance = trip[4].to_f
-    trip_duration = ((Time.parse(trip[3]) - Time.parse(trip[2])) / 3600)
-    average_speed = (trip_distance / trip_duration)
-    return {distance: trip_distance, time: trip_duration, speed: average_speed}
-  end
-
-def assign_trips(hash, driver, new_line)
-   hash.keys.include?(driver) ? hash[driver] << get_trips(new_line) : nil
+def build_trips(trip)
+  trip = trip.split(" ")
+  trip_distance = trip[4].to_f
+  trip_duration = ((Time.parse(trip[3]) - Time.parse(trip[2])) / 3600)
+  average_speed = (trip_distance / trip_duration)
+  return {distance: trip_distance, time: trip_duration, speed: average_speed}
 end
 
-def average_trips(hash)
+def assign_trips_to_driver(hash, driver, new_line)
+   hash.keys.include?(driver) ? hash[driver] << build_trips(new_line) : nil
+end
+
+def map_average_trips_for_drivers(hash)
   hash.map { |driver|
     driver[1].inject {|a, b|
       result = {}
@@ -33,46 +33,41 @@ def average_trips(hash)
       result[:speed] = (result[:distance] / result[:time])
       hash[driver[0]] = [result] }
   }
-
 end
 
-def sort_drivers(hash)
-
-end
-
-def print_trips(hash)
+def output_data(hash)
   array_drivers = []
   hash.each do |driver|
    if !driver[1].empty?
       driver = driver.flatten
       array_drivers << "#{driver[0]}: #{driver[1][:distance].round} miles @ #{driver[1] [:speed].round} mph"
-    else
+   else
       array_drivers << "#{driver[0]}: 0 miles"
   end
 end
-  sort_all_trips(array_drivers).each { |driver|
+  sort_drivers_by_distance(array_drivers).each { |driver|
     puts "#{driver}"
   }
 end
 
-def sort_all_trips(array_drivers)
+def sort_drivers_by_distance(array_drivers)
   sorted_trips = array_drivers.sort { |a, b|
     b[/\d+/].to_i <=> a[/\d+/].to_i }
 end
 
-def extract_data(file)
+def extract_data_from_file(file)
   hash = {}
   parse_file(file).map { |new_line|
     if new_line.include?("Driver")
-      hash[get_drivers(new_line)] = []
+      hash[get_driver_names(new_line)] = []
     elsif new_line.include?("Trip")
       arr = new_line.split(" ")
       driver = arr[1]
-      assign_trips(hash, driver, new_line)
+      assign_trips_to_driver(hash, driver, new_line)
     end
   }
-    average_trips(hash)
-    print_trips(hash)
+    map_average_trips_for_drivers(hash)
+    output_data(hash)
 end
 
-extract_data("seed_data.txt")
+extract_data_from_file("seed_data.txt")
